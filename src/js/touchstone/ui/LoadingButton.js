70bde4c8849861bd4d18695b73aac67d45b8a936
Joss Mackison <joss.mackison@gmail.com>
2014-11-25 16:02:41 +1100

    Improve the loading button component

--- src/js/touchstone/ui/LoadingButton.js
+++ src/js/touchstone/ui/LoadingButton.js
@@ -1,4 +1,5 @@
 var React = require('react/addons'),
+	SetClass = require('classnames'),
 	Tappable = require('../../touchstone/tappable'),
 	Navigation = require('../../touchstone/navigation');
 
@@ -9,20 +10,32 @@ module.exports = React.createClass({
 		showView: React.PropTypes.string,
 		viewTransition: React.PropTypes.string,
 		viewProps: React.PropTypes.object,
-		action: React.PropTypes.func,
+		component: React.PropTypes.string,
+		onTap: React.PropTypes.func,
+		loading: React.PropTypes.bool,
 		label: React.PropTypes.string
 	},
 	getDefaultProps: function() {
 		return {
+			component: 'button',
 			className: ''
 		};
 	},
 	render: function() {
-		var className = this.props.className + ' loading-button';
+		// Class Name
+		var className = SetClass({
+			'loading-button': true,
+			'is-loading': this.props.loading
+		});
+		className += this.props.className ? (' ' + this.props.className) : '';
+
+		// Set Variables
 		var label = this.props.label ? <div className="loading-button-text">{this.props.label}</div> : null;
-		var action = this.props.showView ? this.showViewFn(this.props.showView, this.props.viewTransition, this.props.viewProps) : this.props.action;
+		var onTap = this.props.showView ? this.showViewFn(this.props.showView, this.props.viewTransition, this.props.viewProps) : this.props.onTap;
+
+		// Output Component
 		return (
-			<Tappable className={className} component="div" onTap={action}>
+			<Tappable className={className} component={this.props.component} onTap={onTap}>
 				<span className="loading-button-icon-wrapper">
 					<span className="loading-button-icon" />
 				</span>

5f8669e10bf9cbfae36ee612b934ec75a82d3fda
Joss Mackison <joss.mackison@gmail.com>
2014-11-26 15:17:03 +1100

    Document and group the components

--- src/js/views/components/index.js
+++ src/js/views/components/index.js
@@ -22,16 +22,29 @@ module.exports = React.createClass({
 					<div className="headerbar-label">Components</div>
 				</UI.FlexBlock>
 				<UI.FlexBlock scrollable>
-					<div className="panel is-first">	
-						<div className="gutter-list">
-							<div className="list-item">Alerts</div>
-							<div className="list-item">Bars</div>
-							<div className="list-item">Buttons</div>
-							<div className="list-item">Keypad</div>
-							<div className="list-item">Passcode</div>
-							<div className="list-item">Lists</div>
-							<Link to="components-feedback" viewTransition="show-from-right" className="list-item list-item-has-chevron">View Feedback</Link>
-						</div>
+					<div className="page-header text-caps">Bars</div>
+					<div className="panel gutter-list">
+						<Link to="components-headerbar" viewTransition="show-from-right" className="list-item list-item-has-chevron">Header Bar</Link>
+						<Link to="components-statusbar" viewTransition="show-from-right" className="list-item list-item-has-chevron">Status Bar</Link>
+						<Link to="components-footerbar" viewTransition="show-from-right" className="list-item list-item-has-chevron">Footer Bar</Link>
+						<Link to="components-actionbar" viewTransition="show-from-right" className="list-item list-item-has-chevron">Action Bar</Link>
+					</div>
+					<div className="page-header text-caps">Lists</div>
+					<div className="panel gutter-list">
+						<div className="list-item">Simple List</div>
+						<div className="list-item">Complex List</div>
+					</div>
+					<div className="page-header text-caps">UI Elements</div>
+					<div className="panel gutter-list">
+						<div className="list-item">Buttons</div>
+						<div className="list-item">Keypad</div>
+						<div className="list-item">Passcode</div>
+					</div>
+					<div className="page-header text-caps">Misc.</div>
+					<div className="panel gutter-list">
+						<Link to="components-feedback" viewTransition="show-from-right" className="list-item list-item-has-chevron">View Feedback</Link>
+						<Link to="components-modal-message" viewTransition="show-from-right" className="list-item list-item-has-chevron">Modal Message</Link>
+						<Link to="components-modal-loading" viewTransition="show-from-right" className="list-item list-item-has-chevron">Modal Loading</Link>
 					</div>
 				</UI.FlexBlock>
 			</UI.FlexLayout>

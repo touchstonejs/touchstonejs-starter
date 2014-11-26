34de9c9eb7b3078a981066360d671c6432533792
Joss Mackison <joss.mackison@gmail.com>
2014-11-27 10:08:38 +1100

    Strict about type

--- src/js/views/component/passcode.js
+++ src/js/views/component/passcode.js
@@ -64,10 +64,7 @@ module.exports = React.createClass({
 		return (
 			<UI.FlexLayout className={this.props.viewClassName}>
 				<UI.FlexBlock height="44px" className="Headerbar">
-					<Link to="home" viewTransition="reveal-from-right" className="Headerbar-button">
-						<div className="Headerbar-button-icon ion-chevron-left" />
-						<div className="Headerbar-button-label">Back</div>
-					</Link>
+					<Link to="home" viewTransition="reveal-from-right" className="Headerbar-button ion-chevron-left" component="button">Back</Link>
 					<div className="Headerbar-label">Enter Passcode</div>
 				</UI.FlexBlock>
 				<UI.FlexBlock>
@@ -90,7 +87,7 @@ module.exports = React.createClass({
 						</div>
 					</div>
 				</UI.FlexBlock>
-				<UI.Keypad action={this.handlePasscode} enableDel={this.state.passcode.length} stowed={this.state.working || this.state.keyboardIsStowed} />
+				<UI.Keypad action={this.handlePasscode} enableDel={Boolean(this.state.passcode.length)} stowed={this.state.working || this.state.keyboardIsStowed} />
 			</UI.FlexLayout>
 		);
 	}

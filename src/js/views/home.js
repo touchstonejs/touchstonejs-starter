35f73ced8235290013fed607674d730e4e483d7a
Joss Mackison <joss.mackison@gmail.com>
2014-11-26 20:36:14 +1100

    Countdown on modal message

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -17,20 +17,36 @@ module.exports = React.createClass({
 	getInitialState: function() {
 		return {
 			modalMessageVisible: false,
-			modalLoadingVisible: false
+			modalLoadingVisible: false,
+			modalMessageText: ''
 		};
 	},
 
 	showMessageModal: function() {
 		console.log('modal should open');
 		this.setState({
-			modalMessageVisible: true
+			modalMessageVisible: true,
+			modalMessageText: 'It will close in 5 seconds'
 		});
 
 		setTimeout(function() {
-			this.setState({
-				modalMessageVisible: false
-			});
+			this.setState({ modalMessageText: 'It will close in 4 seconds' });
+		}.bind(this), 1000);
+
+		setTimeout(function() {
+			this.setState({ modalMessageText: 'It will close in 3 seconds' });
+		}.bind(this), 2000);
+
+		setTimeout(function() {
+			this.setState({ modalMessageText: 'It will close in 2 seconds' });
+		}.bind(this), 3000);
+
+		setTimeout(function() {
+			this.setState({ modalMessageText: 'It will close in 1 second' });
+		}.bind(this), 4000);
+
+		setTimeout(function() {
+			this.setState({ modalMessageVisible: false });
 		}.bind(this), 5000);
 	},
 
@@ -41,53 +57,47 @@ module.exports = React.createClass({
 		});
 
 		setTimeout(function() {
-			this.setState({
-				modalLoadingVisible: false
-			});
+			this.setState({ modalLoadingVisible: false });
 		}.bind(this), 5000);
 	},
 
 	render: function() {
 
-		var modalMessageClass = !this.state.modalMessageVisible ? 'hidden' : '';
-
 		return (
 			<UI.FlexLayout className={this.props.viewClassName}>
-				<UI.FlexBlock height="44px" className="Headerbar">
+				<UI.FlexBlock height="44px" className="Headerbar primary">
 					<div className="Headerbar-label">TouchstoneJS</div>
 				</UI.FlexBlock>
 				<UI.FlexBlock scrollable>
 					<div className="view-header text-caps">Bars</div>
 					<div className="panel ios-list">
-						<Link to="components-headerbar" viewTransition="show-from-right" className="list-item is-tappable">Header Bar</Link>
-						<Link to="components-alertbar" viewTransition="show-from-right" className="list-item is-tappable">Alert Bar</Link>
-						<Link to="components-actionbar" viewTransition="show-from-right" className="list-item is-tappable">Action Bar</Link>
-						<Link to="components-footerbar" viewTransition="show-from-right" className="list-item is-tappable">Footer Bar</Link>
+						<Link component="div" to="component-headerbar" viewTransition="show-from-right" className="list-item is-tappable">Header Bar</Link>
+						<Link component="div" to="component-alertbar" viewTransition="show-from-right" className="list-item is-tappable">Alert Bar</Link>
+						<Link component="div" to="component-actionbar" viewTransition="show-from-right" className="list-item is-tappable">Action Bar</Link>
+						<Link component="div" to="component-footerbar" viewTransition="show-from-right" className="list-item is-tappable">Footer Bar</Link>
 					</div>
 					<div className="view-header text-caps">Lists</div>
 					<div className="panel ios-list">
-						<Link to="components-simple-list" viewTransition="show-from-right" className="list-item is-tappable">Simple List</Link>
-						<Link to="components-complex-list" viewTransition="show-from-right" className="list-item is-tappable">Complex List</Link>
-						<Link to="components-header-list" viewTransition="show-from-right" className="list-item is-tappable">Header List</Link>
+						<Link component="div" to="component-simple-list" viewTransition="show-from-right" className="list-item is-tappable">Simple List</Link>
+						<Link component="div" to="component-complex-list" viewTransition="show-from-right" className="list-item is-tappable">Complex List</Link>
+						<Link component="div" to="component-header-list" viewTransition="show-from-right" className="list-item is-tappable">Categorised List</Link>
 					</div>
 					<div className="view-header text-caps">UI Elements</div>
 					<div className="panel ios-list">
-						<div className="list-item">Buttons</div>
-						<div className="list-item">Keypad</div>
-						<div className="list-item">Passcode</div>
+						<Link component="div" to="component-passcode" viewTransition="show-from-right" className="list-item is-tappable">Passcode / Keypad</Link>
 					</div>
-					<div className="view-header text-caps">Scaffolding</div>
+					<div className="view-header text-caps">Application State</div>
 					<div className="panel ios-list">
-						<Link to="transitions" viewTransition="show-from-right" className="list-item is-tappable">View Transitions</Link>
-						<Link to="components-feedback" viewTransition="show-from-right" className="list-item is-tappable">View Feedback</Link>
+						<Link component="div" to="transitions" viewTransition="show-from-right" className="list-item is-tappable">View Transitions</Link>
+						<Link component="div" to="component-feedback" viewTransition="show-from-right" className="list-item is-tappable">View Feedback</Link>
 					</div>
 					<div className="view-header text-caps">Modals</div>
 					<div className="panel ios-list">
-						<Tappable onTap={this.showMessageModal} className="list-item is-tappable">Modal Message</Tappable>
-						<Tappable onTap={this.showLoadingModal} className="list-item is-tappable">Modal Loading</Tappable>
+						<Tappable component="div" onTap={this.showMessageModal} className="list-item is-tappable">Modal Message</Tappable>
+						<Tappable component="div" onTap={this.showLoadingModal} className="list-item is-tappable">Modal Loading</Tappable>
 					</div>
 				</UI.FlexBlock>
-				<UI.Modal header="This is a modal" text="It will close in 5 seconds" visible={this.state.modalMessageVisible} className="text-center" />
+				<UI.Modal header="This is a modal" text={this.state.modalMessageText} visible={this.state.modalMessageVisible} className="text-center" />
 				<UI.Modal text="Loading..." iconKey="ion-load-d" iconType="default" visible={this.state.modalLoadingVisible} className="Modal-loading" />
 			</UI.FlexLayout>
 		);

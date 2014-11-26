1ccf325cc95092cf2ed78401d53f6e3a021d3657
Joss Mackison <joss.mackison@gmail.com>
2014-11-26 16:11:11 +1100

    Abstract data from views

---
+++ src/data/months.js
@@ -0,0 +1,14 @@
+module.exports = [
+	{ name: 'December',   number: '12',  season: 'Summer' },
+	{ name: 'January',    number: '1',   season: 'Summer' },
+	{ name: 'February',   number: '2',   season: 'Summer' },
+	{ name: 'March',      number: '3',   season: 'Autumn' },
+	{ name: 'April',      number: '4',   season: 'Autumn' },
+	{ name: 'May',        number: '5',   season: 'Autumn' },
+	{ name: 'June',       number: '6',   season: 'Winter' },
+	{ name: 'July',       number: '7',   season: 'Winter' },
+	{ name: 'August',     number: '8',   season: 'Winter' },
+	{ name: 'September',  number: '9',   season: 'Spring' },
+	{ name: 'October',    number: '10',  season: 'Spring' },
+	{ name: 'November',   number: '11',  season: 'Spring' }
+];

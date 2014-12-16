d245bc9f14c452feee2fb07784d4d507a7ea0639
Joss Mackison <joss.mackison@gmail.com>
2014-12-16 12:54:19 +1100

    Use dialog mixin to showcase

--- src/js/views/component/passcode.js
+++ src/js/views/component/passcode.js
@@ -1,16 +1,21 @@
 /** @jsx React.DOM */
 
 var React = require('react'),
+	Dialogs = require('touchstonejs').Dialogs,
+	Navigation = require('touchstonejs').Navigation,
 	UI = require('touchstonejs').UI;
 
 module.exports = React.createClass({
+	mixins: [Navigation, Dialogs],
 
 	getInitialState: function() {
 		return {}
 	},
 
-	handlePasscode: function(passCode) {
-		alert('Your passcode is "' + passCode + '".');
+	handlePasscode: function(passcode) {
+		return this.showAlertDialog({ title: 'Alert with Callback', message: 'Your passcode is "' + passcode + '".' }, function() {
+			this.showView('home', 'fade')
+		}.bind(this));
 	},
 
 	render: function() {

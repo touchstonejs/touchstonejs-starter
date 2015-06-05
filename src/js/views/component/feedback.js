545a81f079287ffd5aaadb3479506943b641c5eb
Daniel Cousens <github@dcousens.com>
2015-06-05 10:30:54 +1000

    feedback: s/iconKey/iconName, fix linter errors
    Removes all unused functionality

--- src/js/views/component/feedback.js
+++ src/js/views/component/feedback.js
@@ -1,26 +1,19 @@
-var React = require('react'),
-	SetClass = require('classnames'),
-	Tappable = require('react-tappable'),
-	Navigation = require('touchstonejs').Navigation,
-	Link = require('touchstonejs').Link,
-	UI = require('touchstonejs').UI;
+var React = require('react');
+var UI = require('touchstonejs').UI;
 
 module.exports = React.createClass({
-	mixins: [Navigation],
-
-	flashAlert: function(alertContent) {
-		alert(alertContent);
+	flashAlert: function (alertContent) {
+		window.alert(alertContent);
 	},
 
-	render: function() {
-
+	render: function () {
 		return (
-			<UI.View className={this.props.viewClassName}>
+			<UI.View>
 				<UI.Headerbar type="default" label="Feedback">
 					<UI.HeaderbarButton showView="home" viewTransition="reveal-from-right" icon="ion-chevron-left" label="Back" />
 				</UI.Headerbar>
 				<UI.ViewContent>
-					<UI.Feedback iconKey="ion-compass" iconType="primary" header="Optional Header" subheader="Subheader, also optional" text="Feedback message copy goes here. It can be of any length." actionText="Optional Action" actionFn={this.flashAlert.bind(this, 'You clicked the action.')} />
+					<UI.Feedback iconName="ion-compass" iconType="primary" header="Optional Header" subheader="Subheader, also optional" text="Feedback message copy goes here. It can be of any length." actionText="Optional Action" actionFn={this.flashAlert.bind(this, 'You clicked the action.')} />
 				</UI.ViewContent>
 			</UI.View>
 		);

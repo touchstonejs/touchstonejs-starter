cb9967a22ffa3d9272c20c4037f30867134f5e32
Joss Mackison <joss.mackison@gmail.com>
2015-07-01 18:19:12 +0200

    inline starring for list items

--- src/js/views/list-complex.js
+++ src/js/views/list-complex.js
@@ -1,17 +1,27 @@
 var Container = require('react-container');
 var Link = require('touchstonejs').Link;
 var React = require('react');
+var Tappable = require('react-tappable');
 var UI = require('touchstonejs').UI;
 
 const PEOPLE = require('../../data/people');
 const scrollable = Container.initScrollable();
 
 var ComplexListItem = React.createClass({
+	doThing () {
+		console.log('star this user')
+	},
 	render () {
 		var person = this.props.person;
 		var firstName = person.name.split(' ').slice(0, -1).join(' ');
 		var lastName = person.name.split(' ').slice(-1).join(' ');
 		var initials = firstName.charAt(0).toUpperCase() + lastName.charAt(0).toUpperCase();
+		// unstarred
+		// var starTap = 'item-note default';
+		// var starIcon = 'item-note-icon ion-lg ion-ios-star-outline';
+		// starred
+		var starTap = 'item-note text-' + ['warning', 'primary', 'error', 'success'][(Math.random() * 4) | 0];
+		var starIcon = 'item-note-icon ion-lg ion-ios-star';
 
 		return (
 			<Link to="tabs:list-details" transition="show-from-right" viewProps={{ person: person, prevView: 'list-complex' }} className="list-item" component="div">
@@ -21,7 +31,9 @@ var ComplexListItem = React.createClass({
 						<div className="item-title">{person.name}</div>
 						<div className="item-subtitle">{person.bio}</div>
 					</div>
-					<UI.ItemNote type="default" label="More" icon="ion-chevron-right" />
+					<Tappable onTap={this.doThing} stopPropagation className={starTap}>
+						<div className={starIcon} />
+					</Tappable>
 				</div>
 			</Link>
 		);

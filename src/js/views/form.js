fc30a3cdfc17fb29a46a16344398d4feb6bd8bf8
Joss Mackison <joss.mackison@gmail.com>
2015-07-29 15:33:28 +1000

    update form view to new group syntax

--- src/js/views/form.js
+++ src/js/views/form.js
@@ -6,6 +6,10 @@ var { UI } = require('touchstonejs');
 
 const scrollable = Container.initScrollable();
 
+// html5 input types for testing
+// omitted: button, checkbox, radio, image, hidden, reset, submit
+const HTML5_INPUT_TYPES = ['color', 'date', 'datetime', 'datetime-local', 'email', 'file', 'month', 'number', 'password', 'range', 'search', 'tel', 'text', 'time', 'url', 'week'];
+
 module.exports = React.createClass({
 	statics: {
 		navigationBar: 'main',
@@ -15,74 +19,99 @@ module.exports = React.createClass({
 			}
 		}
 	},
+	
 	getInitialState () {
 		return {
 			flavour: 'chocolate',
 			switchValue: true
 		}
 	},
+	
 	handleFlavourChange (newFlavour) {
 		this.setState({
 			flavour: newFlavour
 		});
 	},
+	
 	handleSwitch (key, event) {
 		var newState = {};
 		newState[key] = !this.state[key];
 
 		this.setState(newState);
 	},
+	
 	alert (message) {
 		dialogs.alert(message, function() {}, null)
 	},
+	
+	renderInputTypes () {
+		return HTML5_INPUT_TYPES.map(type => {
+			return <UI.LabelInput type={type} label={type} placeholder={type} />;
+		});
+	},
+	
 	render () {
 
 		return (
 			<Container scrollable={scrollable}>
-				<UI.GroupHeader>Checkbox</UI.GroupHeader>
+				{/*<UI.Group>
+					<UI.GroupHeader>Input Type Experiment</UI.GroupHeader>
+					<UI.GroupBody>
+						{this.renderInputTypes()}
+					</UI.GroupBody>
+				</UI.Group>*/}
 				<UI.Group>
-					<UI.Item>
-						<UI.ItemInner>
-							<UI.FieldLabel>Switch</UI.FieldLabel>
-							<UI.Switch onTap={this.handleSwitch.bind(this, 'switchValue')} on={this.state.switchValue} />
-						</UI.ItemInner>
-					</UI.Item>
-					<UI.Item>
-						<UI.ItemInner>
-							<UI.FieldLabel>Disabled</UI.FieldLabel>
-							<UI.Switch disabled />
-						</UI.ItemInner>
-					</UI.Item>
+					<UI.GroupHeader>Checkbox</UI.GroupHeader>
+					<UI.GroupBody>
+						<UI.Item>
+							<UI.ItemInner>
+								<UI.FieldLabel>Switch</UI.FieldLabel>
+								<UI.Switch onTap={this.handleSwitch.bind(this, 'switchValue')} on={this.state.switchValue} />
+							</UI.ItemInner>
+						</UI.Item>
+						<UI.Item>
+							<UI.ItemInner>
+								<UI.FieldLabel>Disabled</UI.FieldLabel>
+								<UI.Switch disabled />
+							</UI.ItemInner>
+						</UI.Item>
+					</UI.GroupBody>
 				</UI.Group>
-				<UI.GroupHeader>Radio</UI.GroupHeader>
 				<UI.Group>
-					<UI.RadioList value={this.state.flavour} onChange={this.handleFlavourChange} options={[
-						{ label: 'Vanilla',    value: 'vanilla' },
-						{ label: 'Chocolate',  value: 'chocolate' },
-						{ label: 'Caramel',    value: 'caramel' },
-						{ label: 'Strawberry', value: 'strawberry' }
-					]} />
+					<UI.GroupHeader>Radio</UI.GroupHeader>
+					<UI.GroupBody>
+						<UI.RadioList value={this.state.flavour} onChange={this.handleFlavourChange} options={[
+							{ label: 'Vanilla',    value: 'vanilla' },
+							{ label: 'Chocolate',  value: 'chocolate' },
+							{ label: 'Caramel',    value: 'caramel' },
+							{ label: 'Strawberry', value: 'strawberry' }
+						]} />
+					</UI.GroupBody>
 				</UI.Group>
-				<UI.GroupHeader>Inputs</UI.GroupHeader>
 				<UI.Group>
-					<UI.Input placeholder="Default" />
-					<UI.Input defaultValue="With Value" placeholder="Placeholder" />
-					<UI.Textarea defaultValue="Longtext is good for bios etc." placeholder="Longtext" />
+					<UI.GroupHeader>Inputs</UI.GroupHeader>
+					<UI.GroupBody>
+						<UI.Input placeholder="Default" />
+						<UI.Input defaultValue="With Value" placeholder="Placeholder" />
+						<UI.Textarea defaultValue="Longtext is good for bios etc." placeholder="Longtext" />
+					</UI.GroupBody>
 				</UI.Group>
-				<UI.GroupHeader>Labelled Inputs</UI.GroupHeader>
 				<UI.Group>
-					<UI.LabelInput type="email" label="Email"   placeholder="your.name@example.com" />
-					<UI.LabelInput type="url"   label="URL"     placeholder="http://www.yourwebsite.com" />
-					<UI.LabelInput noedit       label="No Edit" defaultValue="Un-editable, scrollable, selectable content" />
-					<UI.LabelSelect label="Flavour" value={this.state.flavour} onChange={this.handleFlavourChange} options={[
-						{ label: 'Vanilla',    value: 'vanilla' },
-						{ label: 'Chocolate',  value: 'chocolate' },
-						{ label: 'Caramel',    value: 'caramel' },
-						{ label: 'Strawberry', value: 'strawberry' },
-						{ label: 'Banana',     value: 'banana' },
-						{ label: 'Lemon',      value: 'lemon' },
-						{ label: 'Pastaccio',  value: 'pastaccio' }
-					]} />
+					<UI.GroupHeader>Labelled Inputs</UI.GroupHeader>
+					<UI.GroupBody>
+						<UI.LabelInput type="email" label="Email"   placeholder="your.name@example.com" />
+						<UI.LabelInput type="url"   label="URL"     placeholder="http://www.yourwebsite.com" />
+						<UI.LabelInput noedit       label="No Edit" defaultValue="Un-editable, scrollable, selectable content" />
+						<UI.LabelSelect label="Flavour" value={this.state.flavour} onChange={this.handleFlavourChange} options={[
+							{ label: 'Vanilla',    value: 'vanilla' },
+							{ label: 'Chocolate',  value: 'chocolate' },
+							{ label: 'Caramel',    value: 'caramel' },
+							{ label: 'Strawberry', value: 'strawberry' },
+							{ label: 'Banana',     value: 'banana' },
+							{ label: 'Lemon',      value: 'lemon' },
+							{ label: 'Pastaccio',  value: 'pastaccio' }
+						]} />
+					</UI.GroupBody>
 				</UI.Group>
 				<UI.Button type="primary" onTap={this.alert.bind(this, 'You clicked the Primary Button')}>
 					Primary Button

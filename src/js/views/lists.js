f6895f6aed5ecd982571bee52b9347844c9c0c97
Joss Mackison <joss.mackison@gmail.com>
2015-07-29 11:08:44 +1000

    update lists to new group syntax

--- src/js/views/lists.js
+++ src/js/views/lists.js
@@ -15,29 +15,35 @@ module.exports = React.createClass({
 	render: function () {
 		return (
 			<Container scrollable>
-				<UI.GroupHeader>Lists</UI.GroupHeader>
 				<UI.Group>
-					<Link to="tabs:list-simple" transition="show-from-right">
-						<UI.Item showDisclosureArrow>
-							<UI.ItemInner>
-								Simple List
-							</UI.ItemInner>
-						</UI.Item>
-					</Link>
-					<Link to="tabs:list-complex" transition="show-from-right">
-						<UI.Item showDisclosureArrow>
-							<UI.ItemInner>
-								Complex List
-							</UI.ItemInner>
-						</UI.Item>
-					</Link>
+					<UI.GroupHeader>Lists</UI.GroupHeader>
+					<UI.GroupBody>
+						<Link to="tabs:list-simple" transition="show-from-right">
+							<UI.Item showDisclosureArrow>
+								<UI.ItemInner>
+									Simple List
+								</UI.ItemInner>
+							</UI.Item>
+						</Link>
+						<Link to="tabs:list-complex" transition="show-from-right">
+							<UI.Item showDisclosureArrow>
+								<UI.ItemInner>
+									Complex List
+								</UI.ItemInner>
+							</UI.Item>
+						</Link>
+					</UI.GroupBody>
 				</UI.Group>
-				<UI.GroupHeader>GroupHeader</UI.GroupHeader>
 				<UI.Group>
-					<UI.GroupBody>Use groups to contain content or lists. Where appropriate a Group should be accompanied by a GroupHeading and optionally a GroupFooter.</UI.GroupBody>
-					<UI.GroupBody>You can use GroupBody for content inside groups. Subsequent GroupBody components will be separated.</UI.GroupBody>
+					<UI.GroupHeader>GroupHeader</UI.GroupHeader>
+					<UI.GroupBody>
+						<UI.GroupInner>
+							<p>Use groups to contain content or lists. Where appropriate a Group should be accompanied by a GroupHeading and optionally a GroupFooter.</p>
+							GroupBody will apply the background for content inside groups.
+						</UI.GroupInner>
+					</UI.GroupBody>
+					<UI.GroupFooter>GroupFooter: useful for a detailed explaination to express the intentions of the Group. Try to be concise - remember that users are likely to read the text in your UI many times.</UI.GroupFooter>
 				</UI.Group>
-				<UI.GroupFooter>GroupFooter: useful for a detailed explaination to express the intentions of the Group. Try to be concise - remember that users are likely to read the text in your UI many times.</UI.GroupFooter>
 			</Container>
 		);
 	}
